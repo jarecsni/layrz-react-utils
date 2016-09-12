16dd0d0a02b75836d993a3f6c184f09c13d63efc
Janos Jarecsni <janos.jarecsni@gmail.com>
2016-09-12 07:58:17 +0100

    Fixed version

--- index.js
+++ index.js
@@ -3,6 +3,15 @@
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
+exports.Styler = exports.version = undefined;
+
+var _Styler = require('./modules/utils/Styler');
+
+var _Styler2 = _interopRequireDefault(_Styler);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var version = '0.1';
 
 exports.version = version;
+exports.Styler = _Styler2.default;

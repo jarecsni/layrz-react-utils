807301de67edf781f741d6d038187380927e6cf4
Janos Jarecsni <janos.jarecsni@gmail.com>
2016-09-13 08:54:11 +0100

    Added test case to cover cascading of properties

--- src/modules/utils/__tests__/StylerTest.js
+++ src/modules/utils/__tests__/StylerTest.js
@@ -10,6 +10,7 @@ describe('Styler', () => {
 		styler = new Styler({
 			someClassName: {
 				someAttribute: 'something',
+				somethingElse: 'something else',
 				$someCondition: {
 					someAttribute: 'something else when someCondition is true'
 				}
@@ -17,13 +18,22 @@ describe('Styler', () => {
 		});
 	});
 	it('Should return the basic style when condition is false', () => {
-		expect(styler.compose('someClassName', {$someCondition: false})).toEqual({someAttribute: 'something'});
+		expect(styler.compose('someClassName', {$someCondition: false})).toEqual({
+			someAttribute: 'something',
+			somethingElse: 'something else'
+		});
 	});
 	it('Should return the basic style when conditional part is undefined', () => {
-		expect(styler.compose('someClassName')).toEqual({someAttribute: 'something'});
+		expect(styler.compose('someClassName')).toEqual({
+			someAttribute: 'something',
+			somethingElse: 'something else'
+		});
 	});
 	it('Should return the basic style composed with conditional part if condition is true', () => {
 		expect(styler.compose('someClassName', {$someCondition: true})).toEqual(
-			{someAttribute: 'something else when someCondition is true'});
+			{
+				somethingElse: 'something else',
+				someAttribute: 'something else when someCondition is true'
+			});
 	});
 });
